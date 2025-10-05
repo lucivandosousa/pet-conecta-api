79b1846da7d3f0603d9ec40abe41edb53267c0af
Lucivando Sousa <lucivandosousa@gmail.com>
2025-10-05 08:21:26 -0300

    hotfix: fixes JSON parser and static file configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+const routes = require('./routes/routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'API de Adoção de Pets - OK' });
